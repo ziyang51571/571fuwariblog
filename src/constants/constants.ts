57c046404421a9c856b3c5f7b8754dc5e216222c
ziyang51571 <zzy13420990401@163.com>
2025-12-08 02:56:54 +0800

    Site updated 周一 2025/12  2:56

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,12 +6,12 @@ export const LIGHT_MODE = "light",
 export const DEFAULT_THEME = AUTO_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 35;
+export const BANNER_HEIGHT = 100;
 export const BANNER_HEIGHT_EXTEND = 30;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
 // The height the main panel overlaps the banner, unit: rem
-export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
+export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 35;
 
 // Page width: rem
 export const PAGE_WIDTH = 75;
